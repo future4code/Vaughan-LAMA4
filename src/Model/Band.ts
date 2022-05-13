58d65d010e8aeaa4b635719c0df171609d5420b9
Ronald <ronald.santiago46@gmail.com>
2022-05-13 16:52:58 -0300

    Create Bands

---
+++ src/Model/Band.ts
@@ -0,0 +1,51 @@
+export interface BandInputDTO {
+    name: string;
+    genre: string;
+    responsible: string;
+    token: string;
+};
+
+export class Band {
+    constructor(
+        private id: string,
+        private name: string,
+        private genre: string,
+        private responsible: string
+
+    ) {};
+
+    getId() {
+        return this.id;
+    };
+
+    getName() {
+        return this.name;
+    };
+
+    getGenre() {
+        return this.genre;
+    };
+
+    getResponsible() {
+        return this.responsible;
+    };
+
+
+    setId(id: string) {
+        this.id = id;
+    };
+
+    setName(name: string) {
+        this.name = name;
+    };
+
+    setGenre(genre: string) {
+        this.genre = genre;
+    };
+
+    setResponsible(responsible: string) {
+        this.responsible = responsible;
+    };
+
+    
+}
